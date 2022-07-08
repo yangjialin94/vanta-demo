e263a9b794458a361ed7a06ca67341041722c337
Jialin Yang <yangjialin94@gmail.com>
2022-07-08 09:43:01 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    // add threeScript
+    useEffect(() => {
+        // mount
+        const threeScript = document.createElement("script")
+        threeScript.setAttribute("id", "threeScript")
+        threeScript.setAttribute(
+            "src",
+            "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
+        )
+        document.getElementsByTagName("head")[0].appendChild(threeScript)
+
+        // unmount
+        return () => {
+            if (threeScript) {
+                threeScript.remove()
+            }
+        }
+    }, [])
+
+    return <Component {...pageProps} />
 }
 
 export default MyApp
